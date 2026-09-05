8766de46adddb1e6725349d65a18c9fa278b3557
jozin1224 <jozin1224@gmail.com>
2026-09-05 11:41:46 -0300

    Keyboard Driver Added

---
+++ Drivers/keyboard.h
@@ -0,0 +1,39 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#define KBD_DATA_PORT 0x60
+#define KBD_STATUS_PORT 0x64
+
+static inline unsigned char inb(unsigned short port) {
+    unsigned char ret;
+    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+static const char kbd_map[128] = {
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
+  '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
+    0,  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',   0,
+ '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',   0, '*',    0,
+  ' ',  0
+};
+
+static inline char get_pressed_key(void) {
+    if ((inb(KBD_STATUS_PORT) & 1) == 0) {
+        return 0;
+    }
+    
+    unsigned char scancode = inb(KBD_DATA_PORT);
+    
+    if (scancode & 0x80) {
+        return 0;
+    }
+    
+    if (scancode < 128) {
+        return kbd_map[scancode];
+    }
+    
+    return 0;
+}
+
+#endif
--- Kernel/kernel.cpp
+++ Kernel/kernel.cpp
@@ -1,9 +1,21 @@
 #include "../Lib/vga.hpp"
-
+#include "../Drivers/keyboard.h"
 extern "C" void kernel_main() {
     char* video_memory = (char*)0xB8000;
     Vga::Clean();
-    Vga::DrawText("Hello Moto");
+    Vga::DrawText("/Dev/sda> ");
     
-    while(true);
+    while(true)
+    {
+        const char Key = get_pressed_key();
+        if (Key != 0)
+        {
+
+            Vga::WriteChar(Key, 0x07);
+            if (Key == '\n')
+            {
+                Vga::DrawText("/Dev/sda> ");
+            }
+        }
+    }
 }
